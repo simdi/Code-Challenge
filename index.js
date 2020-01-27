14ec8f1694d76b1ade20d513073a47b8a7d3ca0f
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-01-27 22:25:38 +0100

    Added swagger to project.

--- index.js
+++ index.js
@@ -6,8 +6,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerJSDoc = require('swagger-jsdoc');
 const mongoose = require('mongoose');
+const swaggerDoc = require('./swagger.json');
 const routes = require('./routes');
 const port = process.env.PORT || 9000;
 require('dotenv').config();
@@ -21,14 +22,56 @@ mongoose.connect('mongodb://localhost:27017/upstack', (err) => {
 });
 
 const app = express();
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const swaggerSpec = swaggerJSDoc(swaggerDoc);
+const swaggerOptions = { explorer: true };
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 app.get('/', (req, res) => { res.send('Hello world'); });
 // API to register a user
+/**
+ * @swagger
+ *
+ * /register:
+ *   post:
+ *     description: Login to the application
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: username
+ *         description: Username to use for login.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User's password.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: login
+ */
 app.post('/register', routes.register);
 // API to verify both registration and updating a record.
+/**
+ * @swagger
+ * 
+ * /users:
+ *  get:
+ *     description: Returns users
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: users
+ *         schema:
+ *           type: array
+ *           items:
+ *             $ref: '#/definitions/User'
+ */
 app.get('/verify', routes.verify);
 // API to update a user's record.
 app.post('/update', routes.update);
